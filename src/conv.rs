7d45552cbc3872e7e5be4e1b8ce54e071d706302
msiglreith <m.siglreith@gmail.com>
2017-11-23 00:02:50 +0100

    Add support for more formats

--- src/conv.rs
+++ src/conv.rs
@@ -9,19 +9,34 @@ pub fn format_from_hal(format: format::Format) -> VkFormat {
     use hal::format::SurfaceType::*;
 
     match format.0 {
+        R5_G6_B5 => match format.1 {
+            Unorm => VK_FORMAT_R5G6B5_UNORM_PACK16,
+            _ => unreachable!(),
+        },
+        R4_G4_B4_A4 => match format.1 {
+            Unorm => VK_FORMAT_R4G4B4A4_UNORM_PACK16,
+            _ => unreachable!(),
+        },
         R8_G8_B8_A8 => match format.1 {
             Unorm => VK_FORMAT_R8G8B8A8_UNORM,
+            Inorm => VK_FORMAT_R8G8B8A8_SNORM,
             Srgb => VK_FORMAT_R8G8B8A8_SRGB,
-            _ => unimplemented!()
+            _ => panic!("format {:?}", format),
         },
         B8_G8_R8_A8 => match format.1 {
             Unorm => VK_FORMAT_B8G8R8A8_UNORM,
+            Inorm => VK_FORMAT_B8G8R8A8_SNORM,
             Srgb => VK_FORMAT_B8G8R8A8_SRGB,
-            _ => unimplemented!()
+            _ => panic!("format {:?}", format),
+        },
+        R16_G16_B16_A16 => match format.1 {
+            Unorm => VK_FORMAT_R16G16B16A16_UNORM,
+            Inorm => VK_FORMAT_R16G16B16A16_SNORM,
+            Float => VK_FORMAT_R16G16B16A16_SFLOAT,
+            _ => panic!("format {:?}", format),
         },
         _ => {
-            println!("\tformat {:?}", format);
-            unimplemented!()
+            panic!("format {:?}", format);
         }
     }
 }
